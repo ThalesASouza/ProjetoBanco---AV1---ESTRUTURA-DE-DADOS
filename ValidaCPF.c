cec44fbce5c86c577544bcdd2d7ae854df3d9ec2
Thales Souza <thalessouza16@yahoo.com.br>
2020-05-07 13:04:14 -0300

    Correção de identação do código no arquivo ValidaCPF

--- ValidaCPF.c
+++ ValidaCPF.c
@@ -2,68 +2,72 @@
 #include <stdio.h>
 
 int validaCPF(char vcpf[12]){
-char *cpf=vcpf;
-int icpf[12];
-int i,somador=0,digito1,result1,result2,digito2,valor;
+
+    char *cpf=vcpf;
+    int icpf[12];
+    int i,somador=0,digito1,result1,result2,digito2,valor;
 
 
 
-//Efetua a conversao de array de char para um array de int.
-for(i=0;i<11;i++)
-{
-icpf[i]=cpf[i]-48;
-}
+    //Efetua a conversao de array de char para um array de int.
+    for(i=0;i<11;i++){
 
-//PRIMEIRO DIGITO.
+        icpf[i]=cpf[i]-48;
 
-for(i=0;i<9;i++)
-{
-somador+=icpf[i]*(10-i);
-}
+    }
 
-result1=somador%11;
+    //PRIMEIRO DIGITO.
 
-if( (result1==0) || (result1==1) )
-{
-digito1=0;
-}
+    for(i=0;i<9;i++){
 
-else
-{
-digito1 = 11-result1;
-}
+        somador+=icpf[i]*(10-i);
 
-//SEGUNDO DIGITO.
+    }
 
-somador=0;
+    result1=somador%11;
 
-for(i=0;i<10;i++)
-{
-somador+=icpf[i]*(11-i);
-}
+    if( (result1==0) || (result1==1) ){
 
-valor=(somador/11)*11;
-result2=somador-valor;
+        digito1=0;
 
-if( (result2==0) || (result2==1) )
-{
-digito2=0;
-}
+    }else{
 
-else
-{
-digito2=11-result2;
-}
+        digito1 = 11-result1;
 
-//RESULTADOS DA VALIDACAO.
+    }
 
-if((digito1==icpf[9]) && (digito2==icpf[10]))
-{
-return 1;
-}
-else
-{
-return 0;
-}
+    //SEGUNDO DIGITO.
+
+    somador=0;
+
+    for(i=0;i<10;i++){
+
+        somador+=icpf[i]*(11-i);
+
+    }
+
+    valor=(somador/11)*11;
+    result2=somador-valor;
+
+    if( (result2==0) || (result2==1) ){
+
+        digito2=0;
+
+    }else{
+
+        digito2=11-result2;
+    }
+
+    //RESULTADOS DA VALIDACAO.
+
+    if((digito1==icpf[9]) && (digito2==icpf[10])){
+
+        return 1;
+
+    }else{
+
+        return 0;
+
+    }
 
 }
